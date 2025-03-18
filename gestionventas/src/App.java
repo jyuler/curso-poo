8761fd215280046c42dff6cb7e7369c28af8f763
Jhan Yuler <jydelapava@universidadean.edu.co>
2025-03-17 20:54:46 -0500

    guardar todos los vededores en un arreglo

--- gestionventas/src/App.java
+++ gestionventas/src/App.java
@@ -5,13 +5,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         String todo = Datos.getDatosVendedores();
         String[] lineas = todo.split("\n");
+        Vendedor[] arrVendedores = new Vendedor[ lineas.length ];
+        int posv = 0;
         for ( String linea : lineas ) {
             String[] campos = linea.split(","); 
             int nv = Integer.parseInt( campos[0] );
             String nombre = campos[1];
             String apellido = campos[2];
             String fecha = campos[3];
-            Vendedor v = new Vendedor(nv, nombre, apellido, fecha);
+            arrVendedores[ posv++ ] = new Vendedor(nv, nombre, apellido, fecha);
         }
+        System.out.println( arrVendedores[0].getFechaIngreso() );
     }
 }
