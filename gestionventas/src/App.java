872669ab6942f82955c9f3fde4594fba0cf1cc18
Jhan Yuler <jhan.delapava@mercadolibre.com.co>
2025-03-18 17:17:06 -0500

    Como quedó en clase

--- gestionventas/src/App.java
+++ gestionventas/src/App.java
@@ -1,57 +1,20 @@
-import java.time.LocalDate;
-import java.util.Arrays;
-
 import co.edu.ean.poo.ventas.Vendedor;
 import co.edu.ean.poo.ventas.datos.Datos;
-import co.edu.ean.poo.ventas.datos.ParseadorDatos;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String vendedores = Datos.getDatosVendedores();
-        String ventas = Datos.getDatosVentas();
-        Vendedor[] arrVendedores = ParseadorDatos.parse(vendedores, ventas);
-        
-        LocalDate fechaInicio = LocalDate.of(2024, 10, 1);
-        LocalDate fechaFin = LocalDate.of(2024, 12, 31);
-        
-        System.out.println("Total ventas: " + totalVentasVendedores(fechaInicio, fechaFin, arrVendedores));
-        System.out.println("Total comisiones: " + totalComisionesVendedores(fechaInicio, fechaFin, arrVendedores));
-    }
-
-    public static long totalVentasVendedores( LocalDate fechaInicio, LocalDate fechaFin, Vendedor[] vendedores ) {
-        long totalVentas = 0;
-        for (Vendedor v : vendedores) {
-            totalVentas += v.totalVentas(fechaInicio, fechaFin);
+        String todo = Datos.getDatosVendedores();
+        String[] lineas = todo.split("\n");
+        Vendedor[] arrVendedores = new Vendedor[ lineas.length ];
+        int posv = 0;
+        for ( String linea : lineas ) {
+            String[] campos = linea.split(","); 
+            int nv = Integer.parseInt( campos[0] );
+            String nombre = campos[1];
+            String apellido = campos[2];
+            String fecha = campos[3];
+            arrVendedores[ posv++ ] = new Vendedor(nv, nombre, apellido, fecha);
         }
-        return totalVentas;
-    }
-
-    public static int totalComisionesVendedores( LocalDate fechaInicio, LocalDate fechaFin, Vendedor[] vendedores ) {
-        int totalComisiones = 0;
-        for (Vendedor v : vendedores) {
-            totalComisiones += v.calcularComision(fechaInicio, fechaFin);
-        }
-        return totalComisiones;
-    }
-
-    // devuelve el top N de vendedores con mayor valor en ventas en el rango de fechas
-    public static Vendedor[] topNVendedores( int top, LocalDate fechaInicio, LocalDate fechaFin, Vendedor[] vendedores ) {
-        Vendedor[] topN = new Vendedor[top];
-        int[] topNVentas = new int[top];
-        for( Vendedor v : vendedores ) {
-            int tvv = v.totalVentas(fechaInicio, fechaFin);
-            for (int i = 0; i < top; i++) {
-                if (tvv > topNVentas[i]) {
-                    for (int j = top-1; j > i; j--) {
-                        topNVentas[j] = topNVentas[j-1];
-                        topN[j] = topN[j-1];
-                    }
-                    topNVentas[i] = tvv;
-                    topN[i] = v;
-                    break;
-                }
-            }
-        }
-        return topN;
+        System.out.println( arrVendedores[0].getFechaIngreso() );
     }
 }
--- gestionventas/src/co/edu/ean/poo/ventas/Vendedor.java
+++ gestionventas/src/co/edu/ean/poo/ventas/Vendedor.java
@@ -1,17 +1,12 @@
 package co.edu.ean.poo.ventas;
 
-import java.time.LocalDate;
-
-import co.edu.ean.poo.comisiones.CalculadoraComisiones;
-
 public class Vendedor {
     private int numeroVendedor;
     private String nombre;
     private String apellido;
-    private LocalDate fechaIngreso;
-    private ListaVentas ventas = new ListaVentas(10);
+    private String fechaIngreso;
 
-    public Vendedor(int nv, String nm, String ap, LocalDate f ) {
+    public Vendedor(int nv, String nm, String ap, String f ) {
         numeroVendedor = nv;
         nombre = nm;
         apellido = ap;
@@ -30,44 +25,8 @@ public class Vendedor {
         return apellido;
     }
 
-    public LocalDate getFechaIngreso() {
+    public String getFechaIngreso() {
         return fechaIngreso;
-    }
-
-    public boolean registrarVenta(Venta venta) {
-        if ( ! venta.getFecha().isBefore(fechaIngreso)) {
-            ventas.agregarVenta(venta);
-            return true;
-        }
-        return false;
-    }
-
-    public void registrarVenta(LocalDate fechaVenta, int valor) {
-        registrarVenta( new Venta(fechaVenta, valor) );
-    }
-
-    public ListaVentas getVentas() {
-        return ventas;
-    }
-
-    public int totalVentas( LocalDate fechaInicio, LocalDate fechaFin ) {
-        int totalVentas = 0;
-        for (int i = 0; i < ventas.cantidadVentas(); i++) {
-            Venta venta = ventas.getVenta(i);
-            if (venta.getFecha().compareTo(fechaInicio) >= 0 && venta.getFecha().compareTo(fechaFin) <= 0) 
-                totalVentas += venta.getValor();
-        }
-        return totalVentas;
-    }
-
-    public int calcularComision( LocalDate fechaInicio, LocalDate fechaFin ) {
-        float comision = 0;
-        for (int i = 0; i < ventas.cantidadVentas(); i++) {
-            Venta venta = ventas.getVenta(i);
-            if (venta.getFecha().isBefore(fechaInicio) || venta.getFecha().isAfter(fechaFin) ) continue;    
-            comision += CalculadoraComisiones.calcularComision(fechaIngreso, venta.getFecha(), venta.getValor() ).comision();
-        }
-        return Math.round(comision);
     }
 
     @Override
